68e39857a97a3c75876b9502dffd8b95fd5ef06b
Jackson <jacksondahl27@gmail.com>
2026-02-12 20:36:21 -0500

    checkpoint

--- scripts/drone-simulator/main.py
+++ scripts/drone-simulator/main.py
@@ -1,4 +1,17 @@
 """Drone load simulator: mocks many drones posting to the ingestion gateway."""
 
-if __name__ == "__main__":
-    print("test")
+import click
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+
+if __name__ == '__main__':
+    hello()
